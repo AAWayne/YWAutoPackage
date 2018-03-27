fadc497ef7bf8776fc84978a9b929925f66e54b8
阿唯不知道 <90candy.com@gmail.com>
2018-03-27 13:53:59 +0800

    iOS项目自动打包脚本

--- AutoPackage.sh
+++ AutoPackage.sh
@@ -75,21 +75,3 @@ cost_time=$[$end_time - $begin_time]
 #调用时间转换函数
 timeTransformation $cost_time "打包"
 
-## 自动上传蒲公英(这里的 uKey、_api_key)获取地址为 https://www.pgyer.com/doc/api#uploadApp
-################### 配置上传参数 _api_key、uKey Start ########
-#pgy_api_key="650b285e9ba 这里自己去查你的_api_key c94d58ac53a"
-#pgy_ukey="1afad5df91 这里自己去查你的_ukey f7305e29"
-################### 配置蒲公英上传参数 End ####################
-## 开始上传
-#echo "============ 正在上传 ${d_filename} 到蒲公英 ======="
-#ipa_file_path="./${pro_name}.ipa"
-#curl -F "file=@${ipa_file_path}" -F "uKey=${pgy_ukey}" -F "_api_key=${pgy_api_key}" https://qiniu-storage.pgyer.com/apiv1/app/upload
-#echo "============ 上传结束 ======="
-#
-## 上传结束时间
-#upload_end_time=$(date +%s)
-## 计算上传时间(秒：s)
-#upload_time=$[$upload_end_time - $end_time]
-##调用时间转换函数
-#timeTransformation $upload_time "上传蒲公英"
-
