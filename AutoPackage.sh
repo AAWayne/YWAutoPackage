65185a90b0b702c568f66d27edcab7c49164edd6
阿唯不知道 <90candy.com@gmail.com>
2018-03-29 14:29:24 +0800

    优化打包脚本
    1、支持自动上传蒲公英
    2、支持自动上传苹果商店

--- AutoPackage.sh
+++ AutoPackage.sh
@@ -1,26 +1,96 @@
-# iOS AutoPackage Script
-# Author: 阿唯不知道
+#!/bin/bash
+# iOS AutoPackage Shell Script
+# Author:  阿唯不知道 <90candy.com @ gmail.com>
 # 脚本使用方法：直接把脚本拖入终端 然后回车键即可执行
 # 注意1：将plist文件夹、打包脚本放到项目的根目录
 # 注意2：不要在等号两边加空格
+# 注意3：请先正确配置需要打包的项目后再来（如果连手动打包都失败的自然自动打包也不会成功）
 
-################## 配置打包参数 Start ####################
-# 项目名称（如 BaseProject.xcworkspace 则填写 BaseProject ）
-pro_name=BaseProject
-# 是否使用了cocoapods(是就填xcworkspace， 没有使用就填xcodeproj)
-pro_suffix=xcworkspace
-# 是Debug版本还是Release版本,填相应的就行了
-pro_environ=Release
-# 根据具体需求去设置相应的plist文件（这里为企业版）
-pro_plist=EnterpriseExportOptions
-#其他打包环境请配置如下名称：
-# EnterpriseExportOptions - 企业版环境
-# AppStoreExportOptions - 发布App Store的
-# AdHocExportOptions - 开发环境
-# DevelopmentExportOptions - 开发环境
-################## 配置打包参数 End ####################
+############################ 参数配置 ###################################
 
-# 以下无需修改，不懂的请慎改
+# 项目全称（一般为BaseProject.xcworkspace 或者 BaseProject.xcodeproj）
+# 注意，使用cocoapods的一般都填写 xxx.xcworkspace 这种格式
+pro_full_name="BaseProject.xcworkspace"
+
+# 自动上传蒲公英(uKey、_api_key)获取地址https://www.pgyer.com/doc/api#uploadApp
+api_key="" # 不上传可不填
+ukey="" # 不上传可不填
+
+# 自动上传苹果商店(不上传可不填)
+# 苹果开发者账号
+apple_id=""
+apple_pwd=""
+############################ 参数配置 ###################################
+
+
+
+printf "
+#######################################################################
+#                              阿唯不知道
+#                       不要因为骄傲而不屑于抄袭
+#         更多内容请访问 https://www.jianshu.com/u/0f7d26d766f4
+#######################################################################
+"
+# 判断配置是否为空
+if [ -z "$pro_full_name" ]; then
+echo "${CWARNING}项目全称不能为空 ${CEND}"
+exit
+fi
+
+myarray=(${pro_full_name//./ })
+for var in ${array[@]}
+do
+echo $var
+done
+pro_name=${myarray[0]}
+pro_suffix=${myarray[1]}
+# 判断项目全称是否配置正确
+if [ "$pro_suffix" != "xcworkspace" ] && [ "$pro_suffix" != "xcodeproj" ]; then
+echo "${CWARNING}项目名称配置错误，请正确配置project_full_name，如：Project.xcworkspace 或 BaseProject.xcodeproj类型${CEND}"
+exit
+fi
+
+# 打包导出类型(默认为苹果商店正式发布版)
+pro_plist="AppStoreExportOptions"
+
+while :; do
+  printf "
+选择你需要打包的类型（企业版只有企业账号才行哦！）
+\t${CMSG}1${CEND}. Enterprise(企业版)
+\t${CMSG}2${CEND}. App Store(正式版)
+\t${CMSG}3${CEND}. AdHoc(测试版)
+\t${CMSG}4${CEND}. Developers(开发版)
+\t${CMSG}q${CEND}. 退出打包脚本
+"
+  read -p "请输入打包类型: " Number
+  if [[ ! $Number =~ ^[1-4,q]$ ]]; then
+    echo "${CFAILURE}输入错误! 只允许输入 1 ~ 4 和 q${CEND}"
+  else
+    case "$Number" in
+    1)
+      pro_plist=EnterpriseExportOptions
+      break
+      ;;
+    2)
+      pro_plist=AppStoreExportOptions
+      break
+      ;;
+    3)
+      pro_plist=AdHocExportOptions
+      break
+      ;;
+    4)
+      pro_plist=DevelopmentExportOptions
+      break
+      ;;
+    q)
+      exit
+      ;;
+    esac
+  fi
+done
+
+# 开始打包操作
 # 开始时间（用于计算打包脚本执行时间）
 begin_time=$(date +%s)
 # 获取系统时间
@@ -34,17 +104,18 @@ d_filename=${PWD##*/} # 打印当前所在目录(basename `pwd`) 或 echo ${d_fi
 # 时间转换函数（秒转分钟）
 timeTransformation()
 {
-if [ $1 -gt 59 ]
-then
-t_min=$[$1 / 60]
-t_second=$[$1 % 60]
-echo "============ 本次$2用时：${t_min}分${t_second}秒 ======="
-else
-echo "============ 本次$2用时：$1秒 ======="
-fi
+    if [ $1 -gt 59 ]
+    then
+    t_min=$[$1 / 60]
+    t_second=$[$1 % 60]
+    echo "============ 本次$2用时：${t_min}分${t_second}秒 ======="
+    else
+    echo "============ 本次$2用时：$1秒 ======="
+    fi
 }
 echo "============ ${d_filename} 打包开始 ======="
-
+# 默认Release版
+pro_environ=Release
 # 如果没有使用cocoapods 反之if会处理
 pro_clean=project
 if [ $pro_suffix == xcworkspace ]
@@ -67,6 +138,8 @@ xcodebuild -exportArchive -archivePath ./${ipa_dir}/${pro_name}.xcarchive -expor
 # 切换到ipa目录去删除${pro_name}.xcarchive包
 cd ./${ipa_dir}
 rm -r ${pro_name}.xcarchive
+# ipa文件路径（用于上传）
+ipa_path="./${pro_name}.ipa"
 echo "============ ${d_filename} 打包完成 ======="
 # 打包结束时间
 end_time=$(date +%s)
@@ -74,4 +147,58 @@ end_time=$(date +%s)
 cost_time=$[$end_time - $begin_time]
 #调用时间转换函数
 timeTransformation $cost_time "打包"
+
+
+########################## 上传蒲公英 #################################
+uploadPGY()
+{
+    # 判断配置是否为空，空则代表不上传
+    if [ -z "$api_key" ] || [ -z "$ukey" ]; then
+    return
+    fi
+    # 上传开始时间
+    upload_start_time=$(date +%s)
+    # 开始上传
+    echo "============ 正在上传 ${d_filename} 到 蒲公英 ======="
+    curl -F "file=@${ipa_path}" -F "uKey=${ukey}" -F "_api_key=${api_key}" https://qiniu-storage.pgyer.com/apiv1/app/upload
+    echo "============ 上传结束 ======="
+    # 上传结束时间
+    upload_end_time=$(date +%s)
+    # 计算上传时间(秒：s)
+    upload_time=$[$upload_end_time - $upload_start_time]
+    #调用时间转换函数
+    timeTransformation $upload_time "上传蒲公英"
+}
+########################## 上传苹果商店 #################################
+uploadAppStore()
+{
+    # 判断配置是否为空，空则代表不上传
+    if [ -z "$apple_id" ] || [ -z "$apple_pwd" ]; then
+    return
+    fi
+    # 上传开始时间
+    upload_start_time=$(date +%s)
+    # 开始上传
+    echo "============ 准备上传 ${d_filename} 到 AppStore ======="
+    altoolPath="/Applications/Xcode.app/Contents/Applications/Application Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Versions/A/Support/altool"
+    # validate（验证）
+    echo "============ ${d_filename} 正在验证IPA包 ======="
+    "$altoolPath" --validate-app -f "$ipa_path" -u "$apple_id" -p "$apple_pwd" -t ios --output-format xml
+    # 上传
+    echo "============ ${d_filename} 验证结束，正在上传中 ======="
+    "$altoolPath" --upload-app -f "$ipa_path" -u "$apple_id" -p "$apple_pwd" -t ios --output-format xml
+    echo "============ ${d_filename} AppStore - 上传完成 ======="
+    # 上传结束时间
+    upload_end_time=$(date +%s)
+    # 计算上传时间(秒：s)
+    upload_time=$[$upload_end_time - $upload_start_time]
+    #调用时间转换函数
+    timeTransformation $upload_time "上传App Store"
+}
+
+# 如果不上传蒲公英或不上传App Store的话 注释下面两句话即可（如果没填写相关账号则不会执行上传操作）
+uploadPGY
+uploadAppStore
+
+
 
--- README.md
+++ README.md
@@ -2,37 +2,127 @@
 iOS项目自动打包脚本
 
 ### 使用方法：
-1、将脚本和autoplist文件夹拖入项目的根目录后
+1、将`AutoPackage.sh` 脚本和`autoplist文件夹`拖入`项目根目录`后
 
-2、再将AutoPackage.sh脚本文件拖入终端回车即可执行自动打包脚本
+2、配置打开编辑`AutoPackage.sh`中的配置参数（若只打包，则只需配置`pro_full_name`即可）
 
-### 自己使用的打包脚本，这里配置为企业版打包环境，其他版本自行修改plist配置(pro_plist=EnterpriseExportOptions)
+3、再将`AutoPackage.sh`脚本文件拖入`终端`回车即可执行自动打包脚本
 
-* 企业环境 - EnterpriseExportOptions
-* 苹果商店 - AppStoreExportOptions
-* 开发环境 - AdHocExportOptions
-* 开发环境 - DevelopmentExportOption
+4、执行后按需求选择打包版本（企业版、正式版、测试版、开发版）
 
-###  若是需要上传到蒲公英，追加以下代码
 
+### 打包脚本核心内容展示
 ```
-# 自动上传蒲公英(这里的 uKey、_api_key)获取地址为 https://www.pgyer.com/doc/api#uploadApp
-################## 配置上传参数 _api_key、uKey Start ########
-pgy_api_key="650b285e9ba 这里自己去查你的_api_key c94d58ac53a"
-pgy_ukey="1afad5df91 这里自己去查你的_ukey f7305e29"
-################## 配置蒲公英上传参数 End ####################
-# 开始上传
-echo "============ 正在上传 ${d_filename} 到蒲公英 ======="
-ipa_file_path="./${pro_name}.ipa"
-curl -F "file=@${ipa_file_path}" -F "uKey=${pgy_ukey}" -F "_api_key=${pgy_api_key}" https://qiniu-storage.pgyer.com/apiv1/app/upload
-echo "============ 上传结束 ======="
+# 开始打包操作
+# 开始时间（用于计算打包脚本执行时间）
+begin_time=$(date +%s)
+# 获取系统时间
+date_string=`date +"%Y-%m-%d_%H-%M-%S"`
+# 获取脚本当前所在目录(即上级目录绝对路径)
+root_dir=$(cd "$(dirname "$0")"; pwd)/
+# 切换到当前脚本的工作目录
+cd ${root_dir}
+# 获取当前目录名称（父文件夹名字）
+d_filename=${PWD##*/} # 打印当前所在目录(basename `pwd`) 或 echo ${d_filename}
+# 时间转换函数（秒转分钟）
+timeTransformation()
+{
+if [ $1 -gt 59 ]
+then
+t_min=$[$1 / 60]
+t_second=$[$1 % 60]
+echo "============ 本次$2用时：${t_min}分${t_second}秒 ======="
+else
+echo "============ 本次$2用时：$1秒 ======="
+fi
+}
+echo "============ ${d_filename} 打包开始 ======="
+# 默认Release版
+pro_environ=Release
+# 如果没有使用cocoapods 反之if会处理
+pro_clean=project
+if [ $pro_suffix == xcworkspace ]
+then
+pro_clean=workspace
+fi
 
+# Clean操作
+xcodebuild clean -${pro_clean} ${pro_name}.${pro_suffix} -scheme ${pro_name} -configuration ${pro_environ}
+# 创建存放 archive和IPA 的文件夹
+cd ../
+ipa_dir=${d_filename}_${date_string}
+mkdir ${ipa_dir}
+cd ./${d_filename}
+# Archive
+xcodebuild archive -${pro_clean} ${pro_name}.${pro_suffix} -scheme ${pro_name} -archivePath ../${ipa_dir}/${pro_name}.xcarchive
+cd ../
+# 导出IPA包
+xcodebuild -exportArchive -archivePath ./${ipa_dir}/${pro_name}.xcarchive -exportOptionsPlist ./${d_filename}/autoplist/${pro_plist}.plist -exportPath ./${ipa_dir}
+# 切换到ipa目录去删除${pro_name}.xcarchive包
+cd ./${ipa_dir}
+rm -r ${pro_name}.xcarchive
+# ipa文件路径（用于上传）
+ipa_path="./${pro_name}.ipa"
+echo "============ ${d_filename} 打包完成 ======="
+# 打包结束时间
+end_time=$(date +%s)
+# 计算打包时间(秒：s)
+cost_time=$[$end_time - $begin_time]
+#调用时间转换函数
+timeTransformation $cost_time "打包"
+
+
+########################## 上传蒲公英 #################################
+uploadPGY()
+{
+# 判断配置是否为空，空则代表不上传
+if [ -z "$api_key" ] || [ -z "$ukey" ]; then
+return
+fi
+# 上传开始时间
+upload_start_time=$(date +%s)
+# 开始上传
+echo "============ 正在上传 ${d_filename} 到 蒲公英 ======="
+curl -F "file=@${ipa_path}" -F "uKey=${ukey}" -F "_api_key=${api_key}" https://qiniu-storage.pgyer.com/apiv1/app/upload
+echo "============ 上传结束 ======="
 # 上传结束时间
 upload_end_time=$(date +%s)
 # 计算上传时间(秒：s)
-upload_time=$[$upload_end_time - $end_time]
+upload_time=$[$upload_end_time - $upload_start_time]
 #调用时间转换函数
 timeTransformation $upload_time "上传蒲公英"
+}
+########################## 上传苹果商店 #################################
+uploadAppStore()
+{
+# 判断配置是否为空，空则代表不上传
+if [ -z "$apple_id" ] || [ -z "$apple_pwd" ]; then
+return
+fi
+# 上传开始时间
+upload_start_time=$(date +%s)
+# 开始上传
+echo "============ 准备上传 ${d_filename} 到 AppStore ======="
+altoolPath="/Applications/Xcode.app/Contents/Applications/Application Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Versions/A/Support/altool"
+# validate（验证）
+echo "============ ${d_filename} 正在验证IPA包 ======="
+"$altoolPath" --validate-app -f "$ipa_path" -u "$apple_id" -p "$apple_pwd" -t ios --output-format xml
+# 上传
+echo "============ ${d_filename} 验证结束，正在上传中 ======="
+"$altoolPath" --upload-app -f "$ipa_path" -u "$apple_id" -p "$apple_pwd" -t ios --output-format xml
+echo "============ ${d_filename} AppStore - 上传完成 ======="
+# 上传结束时间
+upload_end_time=$(date +%s)
+# 计算上传时间(秒：s)
+upload_time=$[$upload_end_time - $upload_start_time]
+#调用时间转换函数
+timeTransformation $upload_time "上传App Store"
+}
+
+# 如果不上传蒲公英或不上传App Store的话 注释下面两句话即可（如果没填写相关账号则不会执行上传操作）
+uploadPGY
+uploadAppStore
+
 ```
 
 ![](http://upload-images.jianshu.io/upload_images/2822163-089602958ae7072a.png)
